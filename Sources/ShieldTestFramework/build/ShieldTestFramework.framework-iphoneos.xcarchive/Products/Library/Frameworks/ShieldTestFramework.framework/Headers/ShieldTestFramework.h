65e71431dea1fa155992047fd3b7d008a34f66fd
vivekgajbe <vpgajbe1991@gmail.com>
2025-02-25 23:36:15 +0530

    update framework name

---
+++ Sources/ShieldTestFramework/build/ShieldTestFramework.framework-iphoneos.xcarchive/Products/Library/Frameworks/ShieldTestFramework.framework/Headers/ShieldTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  ShieldTestFramework.h
+//  ShieldTestFramework
+//
+//  Created by vivek on 25/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShieldTestFramework.
+FOUNDATION_EXPORT double ShieldTestFrameworkVersionNumber;
+
+//! Project version string for ShieldTestFramework.
+FOUNDATION_EXPORT const unsigned char ShieldTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShieldTestFramework/PublicHeader.h>
+
+
