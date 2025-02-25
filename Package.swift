4f7a18842926ebd2d96b38c18969b90a5aca4a9a
vivekgajbe <vpgajbe1991@gmail.com>
2025-02-25 23:16:04 +0530

    add package file

--- Package.swift
+++ Package.swift
@@ -5,20 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "ShieldTestLibrary",
+    platforms: [
+           .iOS(.v12) // iOS version target
+       ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ShieldTestLibrary",
-            targets: ["ShieldTestLibrary"]),
+            targets: ["ShieldTestSDK"]),
     ],
     targets: [
+        // The destination of the framework you will obfuscate the code
+    .binaryTarget(name: "ShieldTestSDK", path: "./Sources/ShieldTestSDK.xcframework"),
+
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        
         .target(
             name: "ShieldTestLibrary"),
         .testTarget(
             name: "ShieldTestLibraryTests",
             dependencies: ["ShieldTestLibrary"]
-        ),
+                    ),
     ]
 )
